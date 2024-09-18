28c4f208bc9dd9563ccf17717a77a4b309325813
LLCinc723 <69838035+LLCinc723@users.noreply.github.com>
2024-09-18 10:44:05 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,12 @@
+ui_print "*******************************************"
+ui_print "Create module files......"
+
+mkdir /data/adb/modules/.rw
+cd /data/adb/modules/.rw
+mkdir system
+cd system
+mkdir upperdir
+mkdir workdir
+ui_print "*******************************************"
+ui_print "File creation completed."
+ui_print "*******************************************"
